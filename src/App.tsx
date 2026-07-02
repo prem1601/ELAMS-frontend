df3878af7924e57dd2618227c76ac8cb01fb2389
Preetam Chinde <preetam.chinde@quantiantech.com>
2026-07-02 17:43:30 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-3xl font-bold text-center pt-10">
+      Hello from React + TypeScript + Tailwind CSS Template with Vite
+    </div>
+  );
+}
+
+export default App;
